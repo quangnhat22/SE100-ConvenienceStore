ab73220edefd9fb6a303b8feaf355dba8d5de4ab
thaoht194 <thaohtgm@gmail.com>
2022-10-22 06:36:37 +0700

    create TableStaffs

--- src/containers/Admin/Products/index.js
+++ src/containers/Admin/Products/index.js
@@ -1,36 +1,50 @@
-import Search from 'antd/lib/input/Search';
-import React from 'react';
-import TableProducts from './components/TableProducts';
+import Search from "antd/lib/input/Search";
+import React from "react";
+import TableProducts from "./components/TableProducts";
 
 const ProductsPage = () => {
-    
   return (
     <>
       <div className="ml-7 mt-5 mr-3 mb-5 flex flex-col justify-between items-center md:flex-row">
-        <p className="font-semibold text-base">Danh sách đội</p>
+        <p className="font-semibold text-base">Danh sách sản phẩm</p>
 
         <div className="search-container flex flex-col items-center md:flex-row">
           <Search
             name="search"
-            placeholder="Tìm kiếm"
+            placeholder="Tìm kiếm..."
             allowClear
             // onSearch={onSearch}
           />
           {/* button search */}
-          <button 
-          className="flex items-center justify-center rounded-md
-                    bg-blue-600 h-8 w-40 p-2 ml-2 text-white  
-                    mt-3 md:mt-0 hover:bg-blue-800" 
-          // onClick={handleAddTeam}
+          <button
+            className="flex items-center justify-center
+                    bg-blue-500 h-8 w-60 p-2 ml-2 text-white  
+                    mt-3 md:mt-0 hover:bg-blue-600 shadow-lg"
+
+            // onClick={handleAddProduct}
           >
-            Thêm đội
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Thêm sản phẩm
           </button>
         </div>
       </div>
-      <TableProducts/>
+      <TableProducts />
       {/* <ModalForm /> */}
     </>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
