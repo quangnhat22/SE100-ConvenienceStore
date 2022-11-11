e891a9f77cf6c3ae5033be24e3eff1db55fa328d
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2022-11-11 16:58:44 +0700

    information detail

--- src/containers/Admin/Products/components/ProductInforDetail.js
+++ src/containers/Admin/Products/components/ProductInforDetail.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import moment from "moment";
 import { PlusOutlined } from "@ant-design/icons";
 import {
   Form,
@@ -18,13 +19,60 @@ import FormCustomed from "../../../../common/Form/FormCustomed";
 const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 const dateFormat = "DD/MM/YYYY";
-const AddProductForm = () => {
+const ProductInforDetail = () => {
   const [form] = Form.useForm();
+  const [enableModify, setEnableModify] = useState(false);
+  const [componentDisabled, setComponentDisabled] = useState(true);
+  const handleEnableModify = () => {
+    setEnableModify(true);
+    setComponentDisabled(false);
+  };
+  const handleCancel = () => {
+    setEnableModify(false);
+    setComponentDisabled(true);
+    onReset();
+  };
+
   const onFinish = (values) => {
     console.log(values);
   };
+
+  const handleModify = () => {};
+
+  const handleClose = () => {};
+  const onReset = () => {
+    form.resetFields();
+  };
+  let product = {
+    id: "1",
+    name: "bánh",
+    buyprice: 10000,
+    sellprice: 11000,
+    tax: 10,
+    expirydate: [
+      moment("1-11-2020", dateFormat),
+      moment("2-12-2022", dateFormat),
+    ],
+    quantity: 20,
+    description: "đây là mô tả",
+  };
+
   return (
-    <FormCustomed name="add_product_form" form={form} onFinish={onFinish}>
+    <FormCustomed
+      name="product_infor_form"
+      initialValues={{
+        product_id: product.id,
+        product_name: product.name,
+        product_buyprice: product.buyprice,
+        product_sellprice: product.sellprice,
+        product_tax: product.tax,
+        product_expiry_date: product.expirydate,
+        product_quantity: product.quantity,
+        product_description: product.description,
+      }}
+      form={form}
+      onFinish={onFinish}
+    >
       <Form.Item name="product_id" label="Mã sản phẩm">
         <Input
           style={{
@@ -43,7 +91,7 @@ const AddProductForm = () => {
           },
         ]}
       >
-        <Input placeholder="Tên sản phẩm" />
+        <Input placeholder="Tên sản phẩm" disabled={componentDisabled} />
       </Form.Item>
       <Form.Item
         name="product_buyprice"
@@ -62,6 +110,7 @@ const AddProductForm = () => {
             width: "50%",
           }}
           placeholder="Giá nhập"
+          disabled={componentDisabled}
         />
       </Form.Item>
       <Form.Item
@@ -81,6 +130,7 @@ const AddProductForm = () => {
             width: "50%",
           }}
           placeholder="Giá bán"
+          disabled={componentDisabled}
         />
       </Form.Item>
       <Form.Item
@@ -101,6 +151,7 @@ const AddProductForm = () => {
             width: "30%",
           }}
           placeholder="Thuế"
+          disabled={componentDisabled}
         />
       </Form.Item>
       <Form.Item
@@ -118,6 +169,7 @@ const AddProductForm = () => {
           }}
           placeholder={["Ngày sản xuất", "Ngày hết hạn"]}
           format={dateFormat}
+          disabled={componentDisabled}
         />
       </Form.Item>
       <Form.Item
@@ -136,17 +188,22 @@ const AddProductForm = () => {
             width: "30%",
           }}
           placeholder="Số lượng"
+          disabled={componentDisabled}
         />
       </Form.Item>
       <Form.Item name="product_description" label="Mô tả">
-        <TextArea rows={4} placeholder="Mô tả" />
+        <TextArea rows={4} placeholder="Mô tả" disabled={componentDisabled} />
       </Form.Item>
       <Form.Item
         name="product_images"
         label="Ảnh sản phẩm"
         valuePropName="fileList"
       >
-        <Upload action="/upload.do" listType="picture-card">
+        <Upload
+          action="/upload.do"
+          listType="picture-card"
+          disabled={componentDisabled}
+        >
           <div>
             <PlusOutlined />
             <div
@@ -159,15 +216,40 @@ const AddProductForm = () => {
           </div>
         </Upload>
       </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          span: 20,
-          offset: 20,
-        }}
-      >
-        <Button htmlType="submit">Submit</Button>
-      </Form.Item>
+
+      {enableModify === false ? (
+        <Form.Item
+          wrapperCol={{
+            span: 26,
+          }}
+          style={{
+            textAlign: "end",
+          }}
+        >
+          <Button
+            className="edit-product-button mr-4 rounded-md sm:text-yellow-600 hover:border-green-500"
+            onClick={handleEnableModify}
+          >
+            Chỉnh sửa
+          </Button>
+          <Button onClick={handleClose}>Đóng</Button>
+        </Form.Item>
+      ) : (
+        <Form.Item
+          wrapperCol={{
+            span: 26,
+          }}
+          style={{
+            textAlign: "end",
+          }}
+        >
+          <Button onClick={handleCancel}>Hủy</Button>
+          <Button onClick={handleModify} htmlType="submit">
+            Lưu
+          </Button>
+        </Form.Item>
+      )}
     </FormCustomed>
   );
 };
-export default AddProductForm;
+export default ProductInforDetail;
