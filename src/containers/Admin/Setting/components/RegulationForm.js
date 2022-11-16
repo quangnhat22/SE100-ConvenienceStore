553a50e83baf6e70f559f4dcb07115e7872c7dc0
quangnhat22 <wuangsotech@gmail.com>
2022-11-16 23:43:50 +0700

    update setting

--- src/containers/Admin/Setting/components/RegulationForm.js
+++ src/containers/Admin/Setting/components/RegulationForm.js
@@ -3,11 +3,58 @@ import { PlusOutlined } from "@ant-design/icons";
 import { Form, Input, Button, DatePicker, InputNumber, Upload } from "antd";
 import FormCustomed from "../../../../common/Form/FormCustomed";
 import { Colorpicker } from "antd-colorpicker";
+import { useDispatch } from "react-redux";
+import { regulationActions } from "../../../../redux/reducer/RegulationSlice";
+import { modalActions } from "../../../../redux/reducer/ModalReducer";
+import moment from "moment";
+import Swal from "sweetalert2";
+
 
 const RegualtionForm = () => {
   const [form] = Form.useForm();
+  const dispatch = useDispatch();
+
   const onFinish = (values) => {
+    if (values.min > values.max) {
+      Swal.fire({
+        width: "400",
+        height: "100",
+        backdrop: "none",
+        icon: "error",
+        title: "Giá trị tối thiểu không được phép lớn hơn giá trị max",
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true,
+      });
+      return;
+    }
     console.log(values);
+    let newRegualtion = {
+      maTrangThai: moment().valueOf(),
+      tenTrangThai: values.tenTrangThai,
+      min: values.min,
+      max: values.max,
+      color: values.color.hex,
+    };
+
+    dispatch(regulationActions.addNewRegulation(newRegualtion));
+
+    setTimeout(() => {
+      dispatch( modalActions.hideModal() );
+    }, 300)
+
+    Swal.fire({
+      width: "400",
+      height: "100",
+      backdrop: "none",
+      icon: "success",
+      title: "Giá trị tối thiểu không được phép lớn hơn giá trị max",
+      showConfirmButton: false,
+      timer: 1000,
+      timerProgressBar: true,
+    });
+
+    
   };
   //const initialValues = { color: { r: 26, g: 14, b: 85, a: 1 } };
 
