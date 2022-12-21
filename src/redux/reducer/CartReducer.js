61233d721d6cc1690966668f4e8ff51d7cea683e
quangnhat22 <wuangsotech@gmail.com>
2022-12-21 20:54:55 +0700

    upload reducer

--- src/redux/reducer/CartReducer.js
+++ src/redux/reducer/CartReducer.js
@@ -19,7 +19,50 @@ const cartSlice = createSlice({
     // dùng tạm
     //input : reader
     addNewCartItem: (state, action) => {
-      state.cartItems = [action.payload, ...state.cartItems];
+      //state.cartItems = [action.payload, ...state.cartItems];
+      let newCartItem = action.payload;
+      //check cart is available
+      if (state.cartItems.Contains(action.payload)) {
+        state.cartItems = state.cartItems.map((cart) => {
+          if (cart.id === newCartItem.id) {
+            newCartItem.number = cart.number + 1;
+            return { ... newCartItem};
+          } else {
+            return cart;
+          }
+        });
+      } else {
+        state.cartItems = [action.payload, ...state.cartItems];
+      }
+
+      
+    },
+    addNewCartItem: (state, action) => {
+      let newCartItem = action.payload;
+      //check cart is available
+      if (state.cartItems.Contains(action.payload)) {
+        state.cartItems = state.cartItems.map((cart) => {
+          if (cart.id === newCartItem.id) {
+            newCartItem.number = cart.number + 1;
+            return { ... newCartItem};
+          } else {
+            return cart;
+          }
+        });
+      } else {
+        state.cartItems = [action.payload, ...state.cartItems];
+      }
+    },
+    reduceCartItem: (state, action) => {
+      let {id} = action.payload;
+      state.cartItems = state.cartItems.map((cart) => {
+        if (cart.id === id) {
+          cart.number = cart.number - 1;
+          return { ... cart.number};
+        } else {
+          return cart;
+        }
+      });
     },
     // input: id
     removeCartItem: (state, action) => {
