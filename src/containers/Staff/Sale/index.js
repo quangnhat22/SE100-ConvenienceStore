0578fc83e92e92748cfdf936a97161f937fb2c2b
quangnhat22 <wuangsotech@gmail.com>
2022-12-24 13:50:49 +0700

    update lib

--- src/containers/Staff/Sale/index.js
+++ src/containers/Staff/Sale/index.js
@@ -1,4 +1,18 @@
-import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  FormHelperText,
+  Grid,
+  IconButton,
+  Paper,
+  TextField,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import ReactToPrint from "react-to-print";
+import {useRef, useState, useEffect } from "react";
 import { UserOutlined } from "@ant-design/icons";
 import { Layout } from "antd";
 import { Content, Header, Footer } from "antd/lib/layout/layout";
@@ -12,6 +26,7 @@ import * as SagaActionTypes from "../../../redux/constants/constant";
 
 const SalePage = () => {
   const dispatch = useDispatch();
+  const componentRef = useRef(null);
   const { listProduct, loading } = useSelector((state) => state.productSlice);
   useEffect(() => {
     dispatch({ type: SagaActionTypes.GET_LIST_PRODUCT_SAGA });
@@ -35,7 +50,15 @@ const SalePage = () => {
             <p className="mb-0 ml-1 text-white">Nguyễn Văn A</p>
           </div>
         </Header>
-
+        <ReactToPrint
+                  trigger={() => (
+                    // <IconButton variant="text" size="large" color="info">
+                    //   <PrintIcon />
+                    // </IconButton>
+                     <a href="#">Print this out!</a>
+                  )}
+                  content={() => componentRef.current}
+                />
         {/* Hóa đơn bán hàng và danh mục sản phẩm đang chọn */}
         <Content className="flex z-0">
           <div className="w-2/3 bg-slate-300">
@@ -46,6 +69,41 @@ const SalePage = () => {
           </div>
         </Content>
       </Layout>
+
+      {/* printer template */}
+      <div style={{ display: "none" }}>
+        <Paper
+          ref={componentRef}
+          sx={{ width: "100%", overflow: "hidden", p: 5 }}
+        >
+          <Typography variant="h3" gutterBottom sx={{ mb: 4 }}>
+            Hoá đơn
+          </Typography>
+          <Typography variant="h3" gutterBottom sx={{ mb: 4 }}>
+            {/* {`Khách hàng / Cơ quan: ${CoQuan} - Địa chỉ: ${DiaChi}`} */}
+          </Typography>
+          <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
+            {
+              `Ngày lập: ${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`
+            }
+          </Typography>
+          {/* <TableRoomPaymentPrint
+            data={}
+            handleDelete={handleDeleteRoom}
+          /> */}
+           <PaymentForm />
+          <Box sx={{ display: "flex", justifyContent: "right" }}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              sx={{ mt: 2 }}
+              color="secondary"
+            >
+              {/* {`${numberWithCommas(Math.round(totalPrice * 100) / 100)} VNĐ`} */}
+            </Typography>
+          </Box>
+        </Paper>
+      </div>
     </>
   );
 };
